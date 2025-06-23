b35213d083b2383a18114743e84ca99fd8624b0f
Arif Hossen <arifhossen10100@gmail.com>
2025-06-23 11:21:03 +0600

    Add files via upload

---
+++ Day-19-JavaScript/array.js
@@ -0,0 +1,30 @@
+const studentName = ["Moni", "Siam" , "Sagar", "Sowmik"]; 
+console.log(studentName.length);
+console.log(studentName[3]);
+
+for (let i = 0; i < studentName.length; i++) {
+    console.log(studentName[i]);
+    
+}
+
+// push = New data in 
+// pop = Remove last one 
+// shift = 
+// unshift 
+// indexOf
+// splice 
+// slice 
+// reverse
+// split
+
+studentName.push("push_add" , "2nd");
+console.log(studentName);
+studentName.pop();
+console.log(studentName);
+studentName.unshift("Sagar","sagarika","Arafa");
+console.log(studentName);
+
+
+
+
+
---
+++ Day-19-JavaScript/loop.js
@@ -0,0 +1,49 @@
+// let n = 9;
+
+// while (n >= 0) {
+//     console.log("Hello World!" + ".");
+//     n--;
+// }
+
+// let a = 1;
+// const g = 2;
+
+// while (a <= 10) {
+//     console.log(g + " X " + a + " = " + g * a);
+//     a++;
+// } 
+
+// For loop 
+// for (let i = 1; i <= 100; i++) {
+//     console.log(i);
+    
+// }
+
+// console.log("==============");
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 7 == 6 && i > 7) {
+//         console.log(i);
+//     }
+    
+// }
+
+console.log("=============="); 
+
+let x =15;
+
+do {
+    console.log(x);
+    x++
+} while (x < 10);
+
+const city = "Dhaka";
+
+for (let i = city.length -1; i >= 0; i--) {
+    console.log(city[i]);
+    
+}
+
+
+
+
