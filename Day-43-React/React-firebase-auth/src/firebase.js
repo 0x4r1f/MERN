ed5eb2f737b7ccdc096e6c8e7784dd0df0570191
Arif Hossen <arifhossen10100@gmail.com>
2025-07-29 10:20:39 +0600

    React-firebase-auth

---
+++ Day-43-React/React-firebase-auth/src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBphsboQrSR491Q3LdLf0kfFuwbK6_c_pc",
+  authDomain: "alkayda-habibi.firebaseapp.com",
+  projectId: "alkayda-habibi",
+  storageBucket: "alkayda-habibi.firebasestorage.app",
+  messagingSenderId: "299903934012",
+  appId: "1:299903934012:web:b9e8292712c4773db1ed39"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+export default app;
